88b02edb3133f2d4f57aab338729d2c77abce128
William de mata da silva <william.xavante@gmail.com>
2022-06-18 19:21:34 -0300

    Aula 32

---
+++ sql-server/Query's/SQLQueryAula30.sql
@@ -0,0 +1,6 @@
+SELECT * FROM tbl_Autores
+
+SELECT 'William' + ' - Curso SQL SERVER' AS NomeCurso -- CONCATENAÇÃO DE STRING
+
+
+SELECT NomeAutor + ' DE '+ SobreNome AS NomeCompleto FROM tbl_Autores 
